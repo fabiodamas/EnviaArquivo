91ab0066dd42ec6337cae6a64601342d325fec57
Fábio Damas <fabio.damas@gmail.com>
2019-09-10 15:59:37 -0300

    Commit inicial

---
+++ src/main/java/io/github/fabiodamas/EnviaArquivoApplication.java
@@ -0,0 +1,13 @@
+package io.github.fabiodamas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviaArquivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnviaArquivoApplication.class, args);
+	}
+
+}
